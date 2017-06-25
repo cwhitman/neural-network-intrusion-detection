b4a6de910f75db23b034c7bc901c006bc6a506ce
cwhitman <calebrwhitman@gmail.com>
2017-06-25 12:18:03 -0600

    Parser returns dictionary of values now.
    Issue #8

--- pcapParser.py
+++ pcapParser.py
@@ -4,7 +4,10 @@ import sys
 
 
 """Contains functions designed to parse pcap files using scapy-python3: https://github.com/phaethon/scapy.
-   Windows machines require winpcap to work with scapy: https://www.winpcap.org/"""
+   Windows machines require winpcap to work with scapy: https://www.winpcap.org/
+
+   After initialization, parsed, normalized pcap file can be accessed through the field pcap_dictionary.
+   """
 class pcapParser():
 
     """Parses the pcap file and stores it in self.pcap_array.
@@ -12,9 +15,110 @@ class pcapParser():
 			pcap_file: A valid pcap file.
 			pcap_files: Additional pcap files."""
     def __init__(self,pcap_file,*pcap_files):
-        self.pcap_array= self.parseFile(pcap_file,*pcap_files)
+        self.pcap_dictionary = {}
+        self.pcap_dictionary_data = {}
+        self.addFileToDictionary(pcap_file,*pcap_files)
 
-    """Parses the pcap file.
+
+
+    """Updates the connections last hour and connections this month values in the pcap_dictionary.
+        Checks to see if it has been an hour and/or month since that value was last updated and then resets the
+        values appropriately.
+       Args:
+            time: The current time as seconds since the epoc.
+        Retuns: self.pcap_dictionary"""
+    def updateConnectionTimes(self,time):
+        for key in self.pcap_dictionary:
+            if __name__ == '__main__':
+                if(time - self.pcap_dictionary_data[key][0] > 3600): #total number of connections last hour
+                    self.pcap_dictionary_data[key][0]=time
+                    self.pcap_dictionary[key][3] = 0
+                if (time - self.pcap_dictionary_data[key][1] > 3600):  # total number of connections last month
+                    self.pcap_dictionary_data[key][1] = time
+                    self.pcap_dictionary[key][4] = 0
+
+    """Adds the pcap files to self.pcap_dictionary.
+       self.pcap_dictionary is a dictionary where key is the source IP and value is
+       the tuple, [Time of last connection, Total number of connections,Average Time between connection,
+       Total number of connections last hour, Total number of connections this month].
+        self.pcap_dictionary_data is used to keep track of things such as hour_last_reset for each entry.
+        It is organized as { ip, [hour_last_reset,month_last_reset] }
+
+        Assuming that dictionary lookup time is constant, this operation is O(n+m+p) where n is the previous size of the dictionary,
+        m is the number of packets in the pcap_files, and p is the size of the resulting dictionary.
+       Args:
+            pcap_files: Any number of valid pcap file names.
+        Retuns: self.pcap_dictionary"""
+    def addFileToDictionary(self,*pcap_files):
+        self.__unNormalizeData__() # un-nomormalize the data so that it can be edited.
+        for pcap_file in pcap_files:
+            with PcapReader(pcap_file) as pcap_reader:
+                last_time = -1
+                for packet in pcap_reader:
+                    try: #Some packets, such as ARP, don't contain a source IP address.
+                        src = packet.payload.src
+                    except AttributeError:
+                        src = "No IP"
+
+                    if(src not in self.pcap_dictionary): #Add ip to dictionary
+                        self.pcap_dictionary[src] = [packet.time,0.0,0.0,0.0,0.0]
+                        self.pcap_dictionary_data[src] = [float(packet.time),float(packet.time)]
+
+                    list = self.pcap_dictionary[src]
+                    list_data = self.pcap_dictionary_data[src]
+
+                    list[1]+=1 #total number of connections
+                    time_diff = packet.time - list[0] #average time between connections
+                    list[2] = (list[2]*list[1] + time_diff)/list[1]
+                    list[0]=packet.time #time of last connection
+                    last_time = packet.time
+                    if(packet.time - list_data[0] > 3600): #total number of connections last hour
+                        list_data[0] = packet.time
+                        list[3] = 0
+                    else:
+                        list[3]+=1
+                    if(packet.time - list_data[1] > 2592000):#total number of connetions last month
+                        list_data[1] = packet.time
+                        list[4] = 0
+                    else:
+                        list[4] += 1
+
+                #Update the connections last hour and connections last month field
+                #based on the time that the last packet arrived.
+                if (last_time != -1):
+                    self.updateConnectionTimes(last_time)
+        self.__normalizeData__() #normalize the data
+        return self.pcap_dictionary
+
+
+
+    """Normalizes all values inside the dictionary using the function 1/(1/+x).
+        Args:
+            dictionary: dictionary produced using addFileToDictionary. Defaults to self.pcap_dictionary.
+        Returns:
+            The same dictionary with all values normalized."""
+    def __normalizeData__(self,dictionary = None):
+        if(dictionary is None):
+            dictionary = self.pcap_dictionary
+        for key in dictionary:
+             dictionary[key]= [1/(1+x) for x in dictionary[key]]
+        return dictionary
+
+    """Un-normalizes all values inside the dictionary assuming that all values were perviously normalized using 1/(1/+x).
+            Args:
+                dictionary: dictionary produced using addFileToDictionary. Defaults to self.pcap_dictionary.
+            Returns:
+                The same dictionary with all values un-normalized."""
+    def __unNormalizeData__(self, dictionary=None):
+        if (dictionary is None):
+            dictionary = self.pcap_dictionary
+        for key in dictionary:
+            dictionary[key] = [1 /x -1 for x in dictionary[key]]
+        return dictionary
+
+
+    """Parses the pcap file into a simple array as opposed to a dictionary.
+       May be removed in the future. Exists right now as an example.
        Args:
             pcap_files: Any number of valid pcap file names.
        Returns: An array of arrays. Each inner array represents a packet.
@@ -41,16 +145,6 @@ class pcapParser():
                 print("%s file parsed" % (pcap_file))
         return return_array
 
-    """Normalizes all values inside the array using the function 1/(1/+x).
-        Args:
-            array: An array of floats.
-        Returns:
-            The same array with all values normalized."""
-    def normalizeData(self,array):
-        for packet in array:
-            packet[:] = [1/(1+x) for x in packet]
-        return array
-
 
 if __name__ == '__main__':
     if(len(sys.argv) <= 1):
@@ -59,4 +153,5 @@ if __name__ == '__main__':
         argvs = sys.argv
         argvs.pop(0)
         pcapPar = pcapParser(*argvs)
-        normalized = pcapPar.normalizeData(pcapPar.pcap_array)
+
+        print(pcapPar.pcap_dictionary)

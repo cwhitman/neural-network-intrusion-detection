9febdfb0b9b372b746729d9a7eff27409bcd0bce
cwhitman <calebrwhitman@gmail.com>
2017-06-26 10:27:43 -0600

    Access dictionary through a method.
    Before the parsed pcap dictionary could be accesses through a field,
    this was changed to a method.

--- pcapParser.py
+++ pcapParser.py
@@ -6,7 +6,7 @@ import sys
 """Contains functions designed to parse pcap files using scapy-python3: https://github.com/phaethon/scapy.
    Windows machines require winpcap to work with scapy: https://www.winpcap.org/
 
-   After initialization, parsed, normalized pcap file can be accessed through the field pcap_dictionary.
+   After initialization, parsed, normalized pcap file can be accessed through the method pcap_dictionary().
    """
 class pcapParser():
 
@@ -15,42 +15,43 @@ class pcapParser():
 			pcap_file: A valid pcap file.
 			pcap_files: Additional pcap files."""
     def __init__(self,pcap_file,*pcap_files):
-        self.pcap_dictionary = {}
-        self.pcap_dictionary_data = {}
+        self.__pcap_dictionary__ = {}
+        self.__pcap_dictionary_data__ = {}
         self.addFileToDictionary(pcap_file,*pcap_files)
 
+    """Returns the normalized pcap file in dictionary form.
+       If the stored pcap dictionary is large, this method may take a while to return as it normalizes the data before returning it.
+       Returns: { ip , [Time of last connection, Total number of connections,Average Time between connection,
+       Total number of connections last hour, Total number of connections this month] }"""
+    def pcap_dictionary(self):
+        return self.__normalizeData__()
 
-
-    """Updates the connections last hour and connections this month values in the pcap_dictionary.
+    """Updates the connections last hour and connections this month values in the __pcap_dictionary__.
         Checks to see if it has been an hour and/or month since that value was last updated and then resets the
         values appropriately.
        Args:
             time: The current time as seconds since the epoc.
-        Retuns: self.pcap_dictionary"""
+        Retuns: self.__pcap_dictionary__"""
     def updateConnectionTimes(self,time):
-        for key in self.pcap_dictionary:
+        for key in self.__pcap_dictionary__:
             if __name__ == '__main__':
-                if(time - self.pcap_dictionary_data[key][0] > 3600): #total number of connections last hour
-                    self.pcap_dictionary_data[key][0]=time
-                    self.pcap_dictionary[key][3] = 0
-                if (time - self.pcap_dictionary_data[key][1] > 3600):  # total number of connections last month
-                    self.pcap_dictionary_data[key][1] = time
-                    self.pcap_dictionary[key][4] = 0
+                if(time - self.__pcap_dictionary_data__[key][0] > 3600): #total number of connections last hour
+                    self.__pcap_dictionary_data__[key][0]=time
+                    self.__pcap_dictionary__[key][3] = 0
+                if (time - self.__pcap_dictionary_data__[key][1] > 3600):  # total number of connections last month
+                    self.__pcap_dictionary_data__[key][1] = time
+                    self.__pcap_dictionary__[key][4] = 0
 
-    """Adds the pcap files to self.pcap_dictionary.
-       self.pcap_dictionary is a dictionary where key is the source IP and value is
+    """Adds the pcap files to self.__pcap_dictionary__.
+       self.__pcap_dictionary__ is a dictionary where key is the source IP and value is
        the tuple, [Time of last connection, Total number of connections,Average Time between connection,
        Total number of connections last hour, Total number of connections this month].
-        self.pcap_dictionary_data is used to keep track of things such as hour_last_reset for each entry.
+        self.__pcap_dictionary_data__ is used to keep track of things such as hour_last_reset for each entry.
         It is organized as { ip, [hour_last_reset,month_last_reset] }
-
-        Assuming that dictionary lookup time is constant, this operation is O(n+m+p) where n is the previous size of the dictionary,
-        m is the number of packets in the pcap_files, and p is the size of the resulting dictionary.
        Args:
             pcap_files: Any number of valid pcap file names.
-        Retuns: self.pcap_dictionary"""
+        Retuns: self.__pcap_dictionary__"""
     def addFileToDictionary(self,*pcap_files):
-        self.__unNormalizeData__() # un-nomormalize the data so that it can be edited.
         for pcap_file in pcap_files:
             with PcapReader(pcap_file) as pcap_reader:
                 last_time = -1
@@ -60,12 +61,12 @@ class pcapParser():
                     except AttributeError:
                         src = "No IP"
 
-                    if(src not in self.pcap_dictionary): #Add ip to dictionary
-                        self.pcap_dictionary[src] = [packet.time,0.0,0.0,0.0,0.0]
-                        self.pcap_dictionary_data[src] = [float(packet.time),float(packet.time)]
+                    if(src not in self.__pcap_dictionary__): #Add ip to dictionary
+                        self.__pcap_dictionary__[src] = [packet.time, 0.0, 0.0, 0.0, 0.0]
+                        self.__pcap_dictionary_data__[src] = [float(packet.time), float(packet.time)]
 
-                    list = self.pcap_dictionary[src]
-                    list_data = self.pcap_dictionary_data[src]
+                    list = self.__pcap_dictionary__[src]
+                    list_data = self.__pcap_dictionary_data__[src]
 
                     list[1]+=1 #total number of connections
                     time_diff = packet.time - list[0] #average time between connections
@@ -87,34 +88,35 @@ class pcapParser():
                 #based on the time that the last packet arrived.
                 if (last_time != -1):
                     self.updateConnectionTimes(last_time)
-        self.__normalizeData__() #normalize the data
-        return self.pcap_dictionary
+        return self.__pcap_dictionary__
 
 
 
     """Normalizes all values inside the dictionary using the function 1/(1/+x).
         Args:
-            dictionary: dictionary produced using addFileToDictionary. Defaults to self.pcap_dictionary.
+            dictionary: dictionary produced using addFileToDictionary. Defaults to self.__pcap_dictionary__.
         Returns:
             The same dictionary with all values normalized."""
     def __normalizeData__(self,dictionary = None):
         if(dictionary is None):
-            dictionary = self.pcap_dictionary
+            dictionary = self.__pcap_dictionary__
+        new_dictionary={}
         for key in dictionary:
-             dictionary[key]= [1/(1+x) for x in dictionary[key]]
-        return dictionary
+             new_dictionary[key]= [1/(1+x) for x in dictionary[key]]
+        return new_dictionary
 
     """Un-normalizes all values inside the dictionary assuming that all values were perviously normalized using 1/(1/+x).
             Args:
-                dictionary: dictionary produced using addFileToDictionary. Defaults to self.pcap_dictionary.
+                dictionary: dictionary produced using addFileToDictionary. Defaults to self.__pcap_dictionary__.
             Returns:
                 The same dictionary with all values un-normalized."""
     def __unNormalizeData__(self, dictionary=None):
         if (dictionary is None):
-            dictionary = self.pcap_dictionary
+            dictionary = self.__pcap_dictionary__
+        new_dictionary = {}
         for key in dictionary:
-            dictionary[key] = [1 /x -1 for x in dictionary[key]]
-        return dictionary
+             new_dictionary[key]= [1/x -1 for x in dictionary[key]]
+        return new_dictionary
 
 
     """Parses the pcap file into a simple array as opposed to a dictionary.
@@ -154,4 +156,4 @@ if __name__ == '__main__':
         argvs.pop(0)
         pcapPar = pcapParser(*argvs)
 
-        print(pcapPar.pcap_dictionary)
+        print(pcapPar.pcap_dictionary())

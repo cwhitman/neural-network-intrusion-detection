50edef04c65fae2a31e0234dbbe1c8c118752ca7
cwhitman <calebrwhitman@gmail.com>
2017-09-18 18:59:35 -0600

    Fix time bug.
    List files are recorded in non-UTC time zones. A time offset had to be
    included to make them comparable to the times in the parser.

--- AttackLabelParser.py
+++ AttackLabelParser.py
@@ -1,6 +1,7 @@
 import re
 import time
 from datetime import datetime
+import calendar
 
 
 class AttackLabels():
@@ -9,25 +10,27 @@ class AttackLabels():
     Methods isAttack and attackName can them be used to get the attack and it's name.
     """
 
-    def __init__(self,file_path):
+    def __init__(self,file_path,timezone_offset):
         """
         Reads in the file and creates an object to remember and handle the attacks.
         Args:
             file_path: string represent a valid tcpdump.list file
+            timezone_offset: Second offset of timezone from UTC.
         """
-
-        self.attacks=[]
 
         # How much error should be expected when comparing the tcpdump file to the tcpdump.list file in seconds.
         self.timeError = 1
 
-        self.readFile(file_path)
+        self.attacks=[]
+
+        self.readFile(file_path,timezone_offset)
 
 
-    def readFile(self,file_path):
+    def readFile(self,file_path,timezone_offset):
         """Reads in a new file and adds it to the current labler.
            Arges:
-                 file_path: string represent a valid tcpdump.list file"""
+                 file_path: string represent a valid tcpdump.list file
+                 timezone_offset: Second offset of timezone from UTC."""
 
         # line number, start date, start time, duration, service, source port, destination port, source ip, destination ip, attack score, names
         file_regex = re.compile("(.+) (.+ .+) (.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+)")
@@ -36,20 +39,19 @@ class AttackLabels():
             for line in file:
 
                 attributes= re.match(file_regex, line).groups()
-                start_time = time.mktime(datetime.strptime(attributes[1], "%m/%d/%Y %H:%M:%S").timetuple())
+                start_time = calendar.timegm(datetime.strptime(attributes[1], "%m/%d/%Y %H:%M:%S").utctimetuple())+timezone_offset*3600
                 stripped_time = datetime.strptime(attributes[2], "%H:%M:%S")
                 stop_time = stripped_time.hour*3600 + stripped_time.minute*60+stripped_time.second+start_time
                 service = attributes[3]
-                src_port = attributes[4]
-                dest_port = attributes[5]
+                sport = attributes[4]
+                dport = attributes[5]
                 src_ip = attributes[6]
                 dest_ip = attributes[7]
                 attack = True if attributes[8]=="1" else False
-                attack_names = attributes[9]
+                attack_names = attributes[9].split(",")
 
                 if attack:
-                    self.attacks.append([start_time,stop_time,src_port,dest_port,src_ip,dest_ip,attack_names])
-
+                    self.attacks.append([start_time,stop_time,sport,dport,src_ip,dest_ip,attack_names])
 
     def isAttack(self,src_ip,dest_ip,sport,dport,time):
         """Returns True if the input information indicates an attack.
@@ -60,18 +62,18 @@ class AttackLabels():
                 dport: destination port
                 time: time of traffic.
             Returns: True if attack occured around the time with the corresponding information."""
-        #print("%s %s %s %s %s %s"%(src_ip,dest_ip,sport,dport,time))
+        #print("%s %s %s %s %s"%(src_ip,dest_ip,sport,dport,time))
         attacks = [x for x in self.attacks if
                    x[0]<=time+self.timeError and
                    time -self.timeError <=x[1] and
-                   sport == x[2] and
-                   dport == x[3] and
+                   str(sport) == x[2] and
+                   str(dport) == x[3] and
                    x[4]==src_ip and
-                   x[5]==dest_ip]
-        if(len(attacks)>0):
-            return True
-        else:
-            return False
+                   x[5]==dest_ip
+                   ]
+        # if len(attacks)>0:
+        #     print(attacks,":",sport,":",dport)
+        return len(attacks)>0
 
     def attackNames(self,src_ip,dest_ip,sport,dport,time):
         """Returns names of attacks with the given information around the time.
@@ -82,15 +84,14 @@ class AttackLabels():
                 dport: destination port
                 time: time of traffic.
             Returns: A list of attack names or an empty list if there were none."""
-        return [x[-1] for x in self.attacks if
+        return [attack for x in self.attacks if
                    x[0]<=time+self.timeError and
                    time -self.timeError <=x[1] and
-                   sport == x[2] and
-                   dport == x[3] and
+                   str(sport) == x[2] and
+                   str(dport) == x[3] and
                    x[4]==src_ip and
-                   x[5]==dest_ip]
-
-
+                   x[5]==dest_ip
+                   for attack in x[-1]]
 
 if __name__ == '__main__':
     labler = AttackLabels("tcpdump.list")

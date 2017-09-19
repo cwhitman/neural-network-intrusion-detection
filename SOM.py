0880ff01d8c47ae25e579cb4cc639bf09885033b
cwhitman <calebrwhitman@gmail.com>
2017-09-18 19:00:54 -0600

    Added plotting functions.

--- SOM.py
+++ SOM.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.lines as mlines
 import re
-
-"""
-A simple test of a self organizing map neural network.
-This article: https://codesachin.wordpress.com/2015/11/28/self-organizing-maps-with-googles-tensorflow/
-was used as a guide. It was updated to match the current version of tensorflow.
-"""
+import math
+import operator
+import datetime
 
 class SOM(object):
+    """
+    A simple test of a self organizing map neural network.
+    This article: https://codesachin.wordpress.com/2015/11/28/self-organizing-maps-with-googles-tensorflow/
+    was used as a guide. It was updated to match the current version of tensorflow.
+    """
 
     # To check if the SOM has been trained
     _trained = False
+
+    # To check if the SOM has labled nuerons.
+    _labeled = False
 
     def __init__(self, m, n, dim, n_iterations=1000, alpha=0.3, sigma=None):
         """
@@ -95,7 +101,7 @@ class SOM(object):
             # Construct the op that will generate a vector with learning
             # rates for all neurons, based on iteration number and location
             # wrt BMU.
-            #Neighborhood functions is a gaussian apodization function, often written as h(n).
+            #Neighborhood function is a gaussian apodization function, often written as h(n).
             #h(n) = e ^ -( || r_i - r_b || ^2 / sigma(n)^2 ) where r_i are the inputs and r_b are the corresponding bmus.
             bmu_distance_squares = tf.reduce_sum(tf.pow(tf.subtract(
                 self._location_vects, tf.stack(
@@ -155,6 +161,185 @@ class SOM(object):
 
         self._trained = True
 
+    def label(self,labeled_tensors):
+        """Labels the neurons with the liklihood that they are attack or normal.
+           Args: labled_tensors: Labled tensors from the pcap parser."""
+        if not self._trained:
+            raise ValueError("SOM not trained yet")
+        #Setting up a grid of labled neurons.
+        self.labels = [ [{} for _ in range (0,self._n)] for _ in range(0,self._m)]
+        for tensor in labeled_tensors:
+            min_index = min([i for i in range(len(self._weightages))],
+                            key=lambda x: np.linalg.norm(tensor[0] -
+                                                         self._weightages[x]))
+            label = self.labels[self._locations[min_index][0]][self._locations[min_index][1]]
+
+            if(tensor[1]): #attack
+                for attack in tensor[2]:
+                    if attack in label:
+                        label[attack]+=1
+                    else:
+                        label[attack]=1
+            else: #normal
+                if "normal" in label:
+                    label["normal"]+=1
+                else:
+                    label["normal"]=1
+
+        #Changing all values into percentages
+        for row in self.labels:
+            for label in row:
+                total_packets = sum(label.values())
+                for attack in label:
+                    label[attack] = label[attack]/total_packets
+        self._labeled = True
+        return self.labels
+
+    def get_tensor_percentages(self,tensor):
+        """Returns the attack percentages for the given tensor.
+           Args: tensor: A tensor from the pcapParser.
+           Returns: The likelihood of each possible attack in dictionary form.
+                    Keys are attacks and values are percent chance of each attack."""
+        if not self._trained:
+            raise ValueError("SOM not trained yet")
+        if not self._labeled:
+            raise ValueError("SOM not labeled yet")
+
+        min_index = min([i for i in range(len(self._weightages))],
+                        key=lambda x: np.linalg.norm(tensor[0] -
+                                                     self._weightages[x]))
+        return self.labels[self._locations[min_index][0]][self._locations[min_index][1]]
+
+    def get_percentages(self):
+        """ Extremely simple identification that labels neurons as normal traffic divided by total traffic.
+            Returns: An array of percentages. If the neuron received zero packets, then the percentage is set to math.nan.
+        """
+        if not self._trained:
+            raise ValueError("SOM not trained yet")
+        if not self._labeled:
+            raise ValueError("SOM not labeled yet")
+
+        percentage_nums = [ [0 for _ in range (0,self._n)] for _ in range(0,self._m)]
+        for row in range(0,self._n):
+            for col in range(0,self._m):
+                if "normal" in self.labels[row][col]:
+                    percentage_nums[row][col] = self.labels[row][col]["normal"]
+                elif len(self.labels[row][col])==0:
+                    percentage_nums[row][col] = math.nan
+                else:
+                    percentage_nums[row][col] = 0
+        return percentage_nums
+
+    def get_accuracy_matrix(self,labeledTensors):
+        """Runs the tensors against the identification and returns the appropriate accuracy matrix.
+          Args:
+            labeledTensors: labeled tensors produced from the pcapParser.
+        Returns: An array representing the confusion matrix.
+                [ [attack identified as attack, attack identified as normal, attack identified as other],
+                  [normal identified as attack, normal identified as normal, normal identifed as other]"""
+
+        percentages = self.get_percentages()
+
+        #Setting up a simple identification array [ attack#, normal#, total]
+        accuracy_matrix = [ [0,0,0], [0,0,0]]
+        for tensor in labeledTensors:
+            min_index = min([i for i in range(len(self._weightages))],
+                            key=lambda x: np.linalg.norm(tensor[0] -
+                                                         self._weightages[x]))
+            percentage = percentages[self._locations[min_index][0]][self._locations[min_index][1]]
+
+            if (tensor[1]): #Packet is an attack
+                if math.isnan(percentage):
+                    accuracy_matrix[0][2] += 1 #attack identifed as other
+                if percentage < 0.5 : #nueron is attack
+                    accuracy_matrix[0][0] += 1 #attack identified as attack
+                else: #nuerson is normal
+                    accuracy_matrix[0][1] += 1 #attack identified as normal
+
+            else: #Packet is normal
+                if math.isnan(percentage):
+                    accuracy_matrix[1][2] += 1 #normal identifed as other
+                if percentage < 0.5: #nueron is attack
+                    accuracy_matrix[1][0] += 1 #normal identifed as attack
+                else: #nueron is normal
+                    accuracy_matrix[1][1] += 1 #normal identifed as normal
+
+
+        return accuracy_matrix
+
+    def plot_heat_map(self):
+        """Plots a heat map of the nuerons """
+        percentages = self.get_percentages()
+        plt.title("Nueron Heat Map")
+        fig = plt.figure(1)
+        ax = plt.gca()
+        # Color map reference: https://matplotlib.org/examples/color/colormaps_reference.html
+        data = np.ma.masked_invalid(percentages)
+        heatmap = ax.pcolor(data, cmap='bwr_r', vmin=0, vmax=1)
+        # plt.imshow(percentages,cmap='bwr_r',vmin=0,vmax=1)
+        ax.patch.set(hatch='x', edgecolor='black')
+        cbar = fig.colorbar(heatmap)
+        # cbar.ax.get_yaxis().set_ticks([])
+        cbar.ax.text(.5, 1.05, "Normal", ha='center', va='center')
+        cbar.ax.text(.5, -0.05, "Attack", ha='center', va='center')
+
+        # Label the neuron. Comment out if labels are too intrusive.
+        for i, row in enumerate(self.labels):
+            for j, label in enumerate(row):
+                if len(label) > 0:
+                    plt.text(j + 0.5, i + 0.5, max(label.items(), key=operator.itemgetter(1))[0],
+                             horizontalalignment='center',
+                             verticalalignment='center',
+                             fontsize=6)
+
+    def plot_line_graph(self,labeled_tensors,attack_labels):
+        """Plots attacks versus time for both the SOM and the attacks in the AttackLabelParser.
+           Plot is created using matplotlib.
+           Args: labeled_tensors: labeled tensors from the pcapParser
+                 attack_labels: AttackLabelParser containing the real attacks."""
+
+        #Plotting for the attack parser.
+        plt.figure(2)
+        plt.title("Attack Versus Time")
+        attack_names = {}
+        last_y = 0
+        start_time = -1
+        stop_time =-1
+        for attack in attack_labels.attacks:
+            if start_time == -1:
+                start_time = attack[0]
+            stop_time = attack[1]
+            for name in attack[-1]:
+                if name not in attack_names:
+                    attack_names[name] = last_y
+                    last_y += 1
+                attack_handle = plt.plot([attack[0], attack[1]], [attack_names[name], attack_names[name]], 'b')
+
+        #Plotting each packet based on the results of the SOM
+        current_attacks={}
+        for tensor in labeled_tensors:
+            if start_time > tensor[3]:
+                start_time = tensor[3]
+            if stop_time < tensor[3]:
+                stop_time =tensor[3]
+            attack_percentages = self.get_tensor_percentages(tensor)
+            most_likely_traffic = max(attack_percentages.items(), key=operator.itemgetter(1))[0]
+            if most_likely_traffic != "normal":
+                starting_attacks = [starting_attack for starting_attack in attack_percentages if starting_attack not in current_attacks and starting_attack]
+                finished_attacks = [finished_attack for finished_attack in current_attacks if finished_attack != most_likely_traffic and finished_attack]
+                for attack in starting_attacks:
+                        current_attacks[attack]=tensor[3]
+                for attack in finished_attacks:
+                    packet_handle = plt.plot([current_attacks[attack], tensor[3]], [attack_names[attack], attack_names[attack]], 'r')
+
+        #Labels
+        time_format = "%b %d, %Y %H:%M"
+        plt.xticks([int(start_time),int(stop_time)],[datetime.datetime.utcfromtimestamp(start_time).strftime(time_format),datetime.datetime.utcfromtimestamp(stop_time).strftime(time_format)],rotation=-10)
+        plt.yticks(range(0, last_y), attack_names.keys())
+        actual_line = mlines.Line2D([], [], color='blue',markersize=15, label='Actual Attacks')
+        found_line = mlines.Line2D([], [], color='red',markersize=15, label='Attacks found by SOM')
+        plt.legend(handles=[actual_line,found_line])
+
 
     def get_centroids(self):
         """
@@ -190,95 +375,6 @@ class SOM(object):
             to_return.append(self._locations[min_index])
 
         return to_return
-
-    def color_map(self,labeledTensors):
-        """Displays which nuerons are being hit using colors.
-           Args:
-                labeledTensors: labeled tensors produced from the pcapParser.
-            Returns: An array of RGBA color values that can be plotted using mat plot lib"""
-
-        if not self._trained:
-            raise ValueError("SOM not trained yet")
-        #Setting up color array
-        colors = [ [[0,0,0,0] for _ in range (0,self._n)] for _ in range(0,self._m)]
-
-        for tensor in labeledTensors:
-            min_index = min([i for i in range(len(self._weightages))],
-                            key=lambda x: np.linalg.norm(tensor[0] -
-                                                         self._weightages[x]))
-            color = colors[self._locations[min_index][0]][self._locations[min_index][1]]
-
-            if(tensor[1]):
-                color[0] = 1.0  # red
-            else:
-                color[2]= 1.0   # blue
-
-            if color[3]<=0.99:
-                color[3]+=0.01 #Alpha value (color intensity)
-        return colors
-
-    def percentageIdentification(self, labeledTensors):
-        """Extremely simple identification that labels nuerson as attack or normal based on how many packets from each category land
-           on each nueron. Nuerons that have no packets are just labled as (0%,0%) right now.
-            Args:
-                labeledTensors: labeled tensors produced from the pcapParser.
-            Returns: An array of tuples representing attack/normal percentages. ( attack%, normal %)
-        """
-        if not self._trained:
-            raise ValueError("SOM not trained yet")
-        #Setting up a simple identification array [ attack#, normal#, total]
-        packet_nums = [ [[0,0,0] for _ in range (0,self._n)] for _ in range(0,self._m)]
-        for tensor in labeledTensors:
-            min_index = min([i for i in range(len(self._weightages))],
-                            key=lambda x: np.linalg.norm(tensor[0] -
-                                                         self._weightages[x]))
-            id = packet_nums[self._locations[min_index][0]][self._locations[min_index][1]]
-            id[2]+=1
-            if(tensor[1]):
-                id[0]+=1
-            else:
-                id[1]+=1
-
-        #Stupid fix for division problem
-        for row in packet_nums:
-            for id in row:
-                if id[2] ==0:
-                    id[2]=1
-        return [ [(id[0]/id[2],id[1]/id[2])for id in row] for row in packet_nums  ]
-
-    def getAccuracyMatrix(self,labeledTensors,percentages):
-        """Runs the tensors against the identification and returns the appropriate accuracy matrix.
-          Args:
-            labeledTensors: labeled tensors produced from the pcapParser.
-            percentages: Percentage of normal and attack traffic at a nueron. Defined as [ (attack%, normal%) ]
-        Returns: An array representing the confusion matrix.
-                [ [attack identified as attack, attack identified as normal, attack identified as other],
-                  [normal identified as attack, normal identified as normal, normal identifed as other]"""
-
-        #Setting up a simple identification array [ attack#, normal#, total]
-        accuracy_matrix = [ [0,0,0], [0,0,0]]
-        for tensor in labeledTensors:
-            min_index = min([i for i in range(len(self._weightages))],
-                            key=lambda x: np.linalg.norm(tensor[0] -
-                                                         self._weightages[x]))
-            percentage = percentages[self._locations[min_index][0]][self._locations[min_index][1]]
-
-            if (tensor[1]): #Packet is an attack
-                if percentage[0] > percentage[1]: #nueron is attack
-                    accuracy_matrix[0][0] += 1 #attack identified as attack
-                elif percentage[1] > percentage[0]: #nuerson is normal
-                    accuracy_matrix[0][1] += 1 #attack identified as normal
-                else:
-                    accuracy_matrix[0][2] += 1 #attack identifed as other
-            else: #Packet is normal
-                if percentage[0] > percentage[1]: #nueron is attack
-                    accuracy_matrix[1][0] += 1 #normal identifed as attack
-                elif percentage[1] > percentage[0]: #nueron is normal
-                    accuracy_matrix[1][1] += 1 #normal identifed as normal
-                else:
-                    accuracy_matrix[1][2] += 1 #normal identifed as other
-
-        return accuracy_matrix
 
 
     def _neuron_locations(self, m, n):
